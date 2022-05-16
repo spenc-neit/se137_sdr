b460626777653ead7578deb6073963426c05c049
spenc-neit <sdroucoulet@email.neit.edu>
2022-05-16 11:07:04 -0400

    w6 assignment + files

--- w6assignment/script.js
+++ w6assignment/script.js
@@ -14,10 +14,10 @@ for (let i = 0; i<tabs.length; i++)
     tabs[i].addEventListener(`click`, function(e){
         for (let i = 0; i<tabs.length; i++)
         {
-            tabs[i].style.backgroundColor=`orange`;
+            tabs[i].style.backgroundColor=`#373737`;
 
         }
-        e.target.style.backgroundColor=`darkcyan`;
+        e.currentTarget.style.backgroundColor=`#161616`;
         document.querySelectorAll(`#breadcrumbs a`)[3].innerHTML = `Tab${i+1}`
 
     })
